38bf8b02aa4a6ba7ec54e4b45ef80042e8fe0864
andre moratore <andremoratore1978@gmail.com>
2022-07-19 21:07:49 -0300

    Rest Testado

---
+++ src/main/java/com/cladivino/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,18 @@
+package com.cladivino.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/Categorias")
+public class CategoriaResource {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		
+		return "REST está funcionando";
+	}
+
+}
